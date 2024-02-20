e2b67c64db174566a70524f788cbc3de6f940218
Isabela Maria <isabela10014@hotmail.com>
2024-02-20 20:07:32 -0300

    feat: configuração do eslint

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+
+const path = require("path")
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname,'styles', "sass")],
+    prependData: `@import 'abstracts/_index.scss';`
+  }
+}
+
+module.exports = nextConfig
+
