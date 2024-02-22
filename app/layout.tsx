34dcf43684acd2961d4471207329361520534c19
Isabela Maria <isabela10014@hotmail.com>
2024-02-22 20:37:03 -0300

    feat: update local de pastas

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
 
-import { Header } from './@components/organisms'
+import { Header } from '@/components/organisms'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 
