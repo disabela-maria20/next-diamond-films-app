485c91071e6357e960cd1e99b7b5c721730b884d
Isabela Maria <isabela10014@hotmail.com>
2024-03-06 17:56:18 -0300

    feat: update data

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,6 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google'
-import type { Metadata } from 'next'
+import { Metadata } from 'next'
 import { Suspense } from 'react'
 
 import { Filme } from '@/components/templetes'
@@ -26,32 +24,15 @@ export async function generateMetadata({
     }
   }
 }
-type DataItem = {
-  page_title?: string
-  property_title?: string
-  content_type?: string
-  site_country?: string
-}
 
 export default async function pageCatalogoFilme({ params: { slug } }: Params) {
   const filme = await getCatalogoFilme(slug)
-  const data: DataItem[] = [
-    { page_title: filme.movie.title },
-    { property_title: filme.movie.title },
-    { content_type: 'Microsite' },
-    { site_country: 'BR' }
-  ]
+
   return (
     <>
       <Suspense fallback="Carregando">
         <Filme movie={filme} />
       </Suspense>
-      <GoogleTagManager
-        gtmId="GTM-ND454GP5"
-        dataLayerName={filme.movie.title}
-        dataLayer={data as any}
-      />
-      <GoogleAnalytics gaId="G-DRBHT7HM35" />
     </>
   )
 }
