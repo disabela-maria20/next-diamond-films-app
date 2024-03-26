415b425a9660b52f9f0114c0ddd244349a2b5b0c
Isabela Maria <isabela10014@hotmail.com>
2024-03-26 15:28:49 -0300

    feat: update scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
 
 import '@/styles/sass/globals.scss'
+import Script from 'next/script'
+
 import { Footer } from '@/components/molecules'
 import { Header } from '@/components/organisms'
 import Providers from '@/utils/providers'
@@ -21,7 +23,18 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={montserrat.className}>
-        <Header />
+      <Header>
+          <Script dangerouslySetInnerHTML={{
+            __html: `(function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:3920910,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `}} />
+        </Header>
         <Providers>
           {children}
         </Providers>
