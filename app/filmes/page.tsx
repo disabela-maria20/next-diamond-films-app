cb9169e761721923e214b7d2e6014bc208353713
Isabela Maria <isabela10014@hotmail.com>
2024-03-26 15:23:18 -0300

    feat: update banner

--- app/filmes/page.tsx
+++ app/filmes/page.tsx
@@ -1,14 +1,13 @@
 import { Metadata } from 'next'
-import dynamic from 'next/dynamic'
 import { Suspense } from 'react'
 
 import { Loading } from '@/components/atoms'
+import Catalogo from '@/components/templetes/Catalogo'
 import { getHome } from '@/utils/server/requests'
 
 export const metadata: Metadata = {
   title: 'Diamond Films - Filmes'
 }
-const Catalogo = dynamic(() => import('@/components/templetes/Catalogo'))
 
 const PageFilmes = async () => {
   const listaFilmes = await getHome()
