ec4f7f06e0bf392f3d11effae845d7b66750e566
Isabela <isabela10014@hotmail.com>
2024-04-04 14:30:04 -0300

    feat: update redirect

--- app/umavida/page.tsx
+++ app/umavida/page.tsx
@@ -1,13 +1,9 @@
 import { redirect } from 'next/navigation'
 import { Metadata } from 'next/types'
-import { Suspense } from 'react'
-
-import { Loading } from '@/components/atoms'
 
 export const metadata: Metadata = {
   title: 'Diamond Films - Uma Vida'
 }
 export default async function pageUmaVida() {
-  const router = redirect(`https://diamondfilms.com.br/uma-vida/`)
-  return <Suspense fallback={<Loading altura={true} />}>{router}</Suspense>
+  return redirect(`https://diamondfilms.com.br/uma-vida/`)
 }
