d6daa77080b28243ec7ba1570b05cb3f443968b9
Isabela M. Ribeiro <isabela10014@hotmail.com>
2024-07-11 17:18:09 -0300

    feat: update codigo

--- components/templetes/Mensagem/index.tsx
+++ components/templetes/Mensagem/index.tsx
@@ -4,38 +4,34 @@ import React, { useState } from 'react'
 
 import Style from './Mensagem.module.scss'
 
-const specialToRegular: Record<string, string> = {
-  '///': 'Q',
-  '⊔': 'W',
-  '—': 'E',
-  Ω: 'R',
-  '⨀': 'T',
-  '∴': 'Y',
-  '⏁': 'U',
-  '：': 'I',
-  L: 'O',
-  '⨪': 'P',
-  '•': 'A',
-  ᘰ: 'S',
-  '⊂': 'D',
-  ᒕ: 'F',
-  '\\\\': 'G',
-  Ո: 'H',
-  '⊓': 'J',
-  '⅂': 'K',
-  Ↄ: 'L',
-  '＋': 'Z',
-  '⨲': 'X',
-  V: 'C',
-  '⊘': 'V',
-  '//': 'B',
-  '⊥': 'N',
-  ꇓ: 'M'
+const regularToSpecial: Record<string, string> = {
+  Q: '///',
+  W: '⊔',
+  E: '—',
+  R: 'Ω',
+  T: '⨀',
+  Y: '∴',
+  U: '⏁',
+  I: '：',
+  O: 'L',
+  P: '⨪',
+  A: '•',
+  S: 'ᘰ',
+  D: '⊂',
+  F: 'ᒕ',
+  G: '\\\\',
+  H: 'Ո',
+  J: '⊓',
+  K: '⅂',
+  L: 'Ↄ',
+  Z: '＋',
+  X: '⨲',
+  C: 'V',
+  V: '⊘',
+  B: '//',
+  N: '⊥',
+  M: 'ꇓ'
 }
-
-const regularToSpecial: Record<string, string> = Object.fromEntries(
-  Object.entries(specialToRegular).map(([key, value]) => [value, key])
-)
 
 const keyboardOrderQWERTY = [
   ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
@@ -62,15 +58,67 @@ const Mensagem = () => {
   const handleToggle = () => {
     setUseSpecialChars(!useSpecialChars)
     setText((prevText) => {
-      return useSpecialChars
-        ? prevText
-            .split('')
-            .map((char) => regularToSpecial[char] || char)
-            .join('')
-        : prevText
-            .split('')
-            .map((char) => specialToRegular[char] || char)
-            .join('')
+      let newText = prevText
+
+      if (useSpecialChars) {
+        newText = newText
+          .replace(/Q/g, '///')
+          .replace(/W/g, '⊔')
+          .replace(/E/g, '—')
+          .replace(/R/g, 'Ω')
+          .replace(/T/g, '⨀')
+          .replace(/Y/g, '∴')
+          .replace(/U/g, '⏁')
+          .replace(/I/g, '：')
+          .replace(/O/g, 'L')
+          .replace(/P/g, '⨪')
+          .replace(/A/g, '•')
+          .replace(/S/g, 'ᘰ')
+          .replace(/D/g, '⊂')
+          .replace(/F/g, 'ᒕ')
+          .replace(/G/g, '\\\\')
+          .replace(/H/g, 'Ո')
+          .replace(/J/g, '⊓')
+          .replace(/K/g, '⅂')
+          .replace(/L/g, 'Ↄ')
+          .replace(/Z/g, '＋')
+          .replace(/X/g, '⨲')
+          .replace(/C/g, 'V')
+          .replace(/V/g, '⊘')
+          .replace(/B/g, '//')
+          .replace(/N/g, '⊥')
+          .replace(/M/g, 'ꇓ')
+      } else {
+        newText = newText
+          .replace(/\/\/\//g, 'Q')
+          .replace(/⊔/g, 'W')
+          .replace(/—/g, 'E')
+          .replace(/Ω/g, 'R')
+          .replace(/⨀/g, 'T')
+          .replace(/∴/g, 'Y')
+          .replace(/⏁/g, 'U')
+          .replace(/：/g, 'I')
+          .replace(/L/g, 'O')
+          .replace(/⨪/g, 'P')
+          .replace(/•/g, 'A')
+          .replace(/ᘰ/g, 'S')
+          .replace(/⊂/g, 'D')
+          .replace(/ᒕ/g, 'F')
+          .replace(/\\\\/g, 'G')
+          .replace(/Ո/g, 'H')
+          .replace(/⊓/g, 'J')
+          .replace(/⅂/g, 'K')
+          .replace(/Ↄ/g, 'L')
+          .replace(/＋/g, 'Z')
+          .replace(/⨲/g, 'X')
+          .replace(/V/g, 'C')
+          .replace(/⊘/g, 'V')
+          .replace(/\/\//g, 'B')
+          .replace(/⊥/g, 'N')
+          .replace(/ꇓ/g, 'M')
+      }
+
+      return newText
     })
   }
 
@@ -96,7 +144,7 @@ const Mensagem = () => {
         <label htmlFor="mensagemInicio">Decifre as mensagens de longlegs</label>
         <textarea
           value={text}
-          onChange={handleChange} // Adiciona o onChange para atualizar o estado `text`
+          onChange={handleChange}
           id="mensagemInicio"
           placeholder="Digite seu texto aqui..."
         />
