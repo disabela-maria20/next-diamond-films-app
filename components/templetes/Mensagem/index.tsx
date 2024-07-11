f04b93463387f89bc061a428eb0bee1e2f96fe5a
Isabela M. Ribeiro <isabela10014@hotmail.com>
2024-07-11 16:53:38 -0300

    feat: update tela de escrever mensagem

--- components/templetes/Mensagem/index.tsx
+++ components/templetes/Mensagem/index.tsx
@@ -4,125 +4,125 @@ import React, { useState } from 'react'
 
 import Style from './Mensagem.module.scss'
 
+const specialToRegular: Record<string, string> = {
+  '///': 'Q',
+  '⊔': 'W',
+  '—': 'E',
+  Ω: 'R',
+  '⨀': 'T',
+  '∴': 'Y',
+  '⏁': 'U',
+  '：': 'I',
+  L: 'O',
+  '⨪': 'P',
+  '•': 'A',
+  ᘰ: 'S',
+  '⊂': 'D',
+  ᒕ: 'F',
+  '\\\\': 'G',
+  Ո: 'H',
+  '⊓': 'J',
+  '⅂': 'K',
+  Ↄ: 'L',
+  '＋': 'Z',
+  '⨲': 'X',
+  V: 'C',
+  '⊘': 'V',
+  '//': 'B',
+  '⊥': 'N',
+  ꇓ: 'M'
+}
+
+const regularToSpecial: Record<string, string> = Object.fromEntries(
+  Object.entries(specialToRegular).map(([key, value]) => [value, key])
+)
+
+const keyboardOrderQWERTY = [
+  ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+  ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+  ['Z', 'X', 'C', 'V', 'B', 'N', 'M']
+]
+
 const Mensagem = () => {
-  const [mensagemInicio, setMensagemInicio] = useState<string>('')
-  const [mensagemFim, setMensagemFim] = useState<string>('')
+  const [text, setText] = useState('')
+  const [useSpecialChars, setUseSpecialChars] = useState(true)
 
-  const alfabetoInicio = 'abcdefghijklmnopqrstuvwxyz'.split('')
-  const alfabetoFim =
-    '•, ∕∕, V, ⊂, —, Г, ∖∖, ∩, :, ⨅, ⏋, ͻ, ⨰, ⏊, ⎿, ⨪, ⫻, Ω, Ͼ, ⊙, ⏁, ⊘, ⊔, ⨲, ∴, ╋'.split(
-      ', '
-    )
+  const handleKeyPress = (char: string) => {
+    setText((prevText) => prevText + char)
+  }
 
-  const transformarParaHtmlEntities = (
-    texto: string,
-    alfabetoOrigem: string[],
-    alfabetoDestino: string[]
-  ): string => {
-    const mapeamentoParaEspecial = new Map<string, string>()
-    const mapeamentoParaNormal = new Map<string, string>()
+  const handleDelete = () => {
+    setText((prevText) => prevText.slice(0, -1))
+  }
 
-    // Construindo os mapas bidirecionais
-    alfabetoOrigem.forEach((char, index) => {
-      mapeamentoParaEspecial.set(char.toLowerCase(), alfabetoDestino[index])
-      mapeamentoParaNormal.set(alfabetoDestino[index], char.toLowerCase())
+  const handleCopy = () => {
+    navigator.clipboard.writeText(text)
+  }
+
+  const handleToggle = () => {
+    setUseSpecialChars(!useSpecialChars)
+    setText((prevText) => {
+      return useSpecialChars
+        ? prevText
+            .split('')
+            .map((char) => regularToSpecial[char] || char)
+            .join('')
+        : prevText
+            .split('')
+            .map((char) => specialToRegular[char] || char)
+            .join('')
     })
-
-    let resultado = ''
-    let temp = ''
-
-    for (const char of texto) {
-      if (mapeamentoParaEspecial.has(char.toLowerCase())) {
-        temp += mapeamentoParaEspecial.get(char.toLowerCase())!
-      } else if (mapeamentoParaNormal.has(char)) {
-        temp += mapeamentoParaNormal.get(char)!
-      } else {
-        if (temp) {
-          resultado += temp
-          temp = ''
-        }
-        resultado += char
-      }
-    }
-
-    if (temp) {
-      resultado += temp
-    }
-
-    return resultado
   }
 
-  const handleMensagemInicioChange = (
-    event: React.ChangeEvent<HTMLTextAreaElement>
-  ) => {
-    const texto = event.target.value
-    setMensagemInicio(texto)
-    const mensagemFimTransformada = transformarParaHtmlEntities(
-      texto,
-      alfabetoInicio,
-      alfabetoFim
-    )
-    setMensagemFim(mensagemFimTransformada)
-  }
+  const keys = useSpecialChars
+    ? keyboardOrderQWERTY.map((row) =>
+        row.map((key) => regularToSpecial[key] || key)
+      )
+    : keyboardOrderQWERTY
 
-  const handleMensagemFimChange = (
-    event: React.ChangeEvent<HTMLTextAreaElement>
-  ) => {
-    const texto = event.target.value
-    setMensagemFim(texto)
-    const mensagemInicioTransformada = transformarParaHtmlEntities(
-      texto,
-      alfabetoFim,
-      alfabetoInicio
-    )
-    setMensagemInicio(mensagemInicioTransformada)
-  }
-
-  const copiarMensagemFim = () => {
-    navigator.clipboard
-      .writeText(mensagemFim)
-      .then(() => {
-        return
-      })
-      .catch((err) => {
-        console.error('Erro ao copiar a mensagem: ', err)
-      })
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(event.target.value)
   }
 
   return (
     <section className={Style.Mensagem}>
-      <img src="/img/longlegs.png" alt="Poster do filme longlegs" />
-      <div className="container">
-        <div className={Style.areaMensagem}>
-          <h1>Longlegs - Vínculo Mortal</h1>
-          <p>
-            Digite uma mensagem no campo abaixo e veja como ela se transforma no
-            código secreto de Longlegs. Ou, se você se sentir corajoso, tente
-            decifrar uma mensagem dele.
-          </p>
-          <div className={Style.grid}>
-            <label htmlFor="mensagemInicio">
-              <span>Escreva sua mensagem</span>
-              <textarea
-                id="mensagemInicio"
-                value={mensagemInicio}
-                onChange={handleMensagemInicioChange}
-              />
-            </label>
-            <label htmlFor="mensagemFim">
-              <span>Sua mensagem ficou</span>
-              <textarea
-                id="mensagemFim"
-                className={Style.font}
-                value={mensagemFim}
-                onChange={handleMensagemFimChange}
-              />
-            </label>
-          </div>
-          <div className={Style.areaBtn}>
-            <button onClick={copiarMensagemFim}>Copiar</button>
-          </div>
+      <div className={Style.areaMensagem}>
+        <h2>
+          29 de agosto
+          <span>SOMENTE NOS CINEMAS</span>
+        </h2>
+        <img src="/img/longlegs/logo.png" alt="logo do filme longlegs" />
+        <h1>Longlegs - Vínculo Mortal</h1>
+        <label htmlFor="mensagemInicio">Decifre as mensagens de longlegs</label>
+        <textarea
+          value={text}
+          onChange={handleChange} // Adiciona o onChange para atualizar o estado `text`
+          id="mensagemInicio"
+          placeholder="Digite seu texto aqui..."
+        />
+        <div className={Style.areaBtn}>
+          <button className={Style.alterar} onClick={handleToggle}>
+            ALTERNE A LINGUAGEM
+          </button>
+          <button className={Style.copiar} onClick={handleCopy}>
+            Copiar
+          </button>
         </div>
+        <div className={Style.letras}>
+          {keys.map((row, rowIndex) => (
+            <div key={rowIndex} className={Style.row}>
+              {row.map((char) => (
+                <button key={char} onClick={() => handleKeyPress(char)}>
+                  {char}
+                </button>
+              ))}
+            </div>
+          ))}
+          <button onClick={handleDelete}>Deletar</button>
+        </div>
+        <p className={Style.classificacao}>
+          VERIFIQUE A CLASSIFICAÇÃO INDICATIVA
+        </p>
       </div>
     </section>
   )
