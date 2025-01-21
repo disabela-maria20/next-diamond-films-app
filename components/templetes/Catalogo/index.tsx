4df4e55444870e3a00dd9d9b3bf4d6ee79ebd495
Isabela M. <isabela10014@hotmail.com>
2025-01-21 11:14:19 -0300

    fix: Correção no titulos de busca na pagina de catalogo "filmes"

--- components/templetes/Catalogo/index.tsx
+++ components/templetes/Catalogo/index.tsx
@@ -10,8 +10,8 @@ import { IFilmeResponse } from '@/utils/server/types'
 
 interface ICatalogoProps {
   listaFilmes: {
-    releases: IFilmeResponse[]
-    streaming: IFilmeResponse[]
+    releases: Array<IFilmeResponse>
+    streaming: Array<IFilmeResponse>
   }
 }
 
@@ -19,48 +19,41 @@ const Catalogo: React.FC<ICatalogoProps> = ({ listaFilmes }) => {
   const [filtroGenero, setFiltroGenero] = useState<string>('')
   const [filtroAno, setFiltroAno] = useState<string>('')
   const [pesquisa, setPesquisa] = useState<string>('')
-  const [resultadosPesquisa, setResultadosPesquisa] = useState<IFilmeResponse[]>([])
-  const [titulo, setTitulo] = useState<string>('TUDO')
+  const [ultimaPesquisa, setUltimaPesquisa] = useState<string>('') // Estado adicional para o título
+  const [filtroPesquisa, setFiltroPesquisa] = useState<IFilmeResponse[]>([])
   const [isLoading, setIsLoading] = useState<boolean>(false)
 
   const { formatAno } = useFormatarData()
+  const lancamento = listaFilmes.releases
+  const streaming = listaFilmes.streaming
+  const concatFilmes = lancamento.concat(streaming)
+
   const { dataLayerMovieFilter } = useGtag()
 
-  const todosOsFilmes = [...listaFilmes.releases, ...listaFilmes.streaming]
-
-  const handleBuscar = () => {
+  const handlePesquisa = () => {
     setIsLoading(true)
+    setUltimaPesquisa(pesquisa) // Atualiza a última pesquisa
+    const filmesPesquisados = concatFilmes.filter(ItemPesquisados)
+      setFiltroPesquisa(filmesPesquisados)
+      setIsLoading(false)
+      dataLayerMovieFilter('Filmes | Diamond Film', 'filmes', '', ultimaPesquisa, 0);
 
-    const filmesFiltrados = todosOsFilmes.filter((filme) =>
-      filme.title.toLowerCase().includes(pesquisa.toLowerCase())
-    )
-    setResultadosPesquisa(filmesFiltrados)
-    atualizarTitulo(filmesFiltrados)
-    setIsLoading(false) // Simula um pequeno atraso
   }
 
-  const atualizarTitulo = (filmes: IFilmeResponse[]) => {
-    if (filmes.length === 0) {
-      setTitulo('Nenhum resultado encontrado');
-      dataLayerMovieFilter('Filmes | Diamond Film', 'filmes', '', 'Nenhum resultado encontrado', 0);
-      return;
-    }
-
-    const tituloAtualizado =
-      filtroGenero ? `Gênero: ${filtroGenero}` :
-        filtroAno ? `Ano: ${filtroAno}` :
-          pesquisa ? `Resultados para: "${pesquisa}"` :
-            'TUDO';
-
-    dataLayerMovieFilter('Filmes | Diamond Film', 'filmes', '', tituloAtualizado, 0);
-
-    setTitulo(tituloAtualizado);
-  };
-
+  const ItemPesquisados = (filme: IFilmeResponse) => {
+    return filme.title.toLowerCase().includes(pesquisa.toLowerCase())
+  }
 
   const filtrarFilmes = (filme: IFilmeResponse) => {
-    if (filtroGenero && filme.genre !== filtroGenero) return false
-    if (filtroAno && formatAno(filme.releasedate) !== parseInt(filtroAno)) return false
+    if (filtroPesquisa.length > 0 && !filtroPesquisa.includes(filme)) {
+      return false
+    }
+    if (filtroGenero && filme.genre !== filtroGenero) {
+      return false
+    }
+    if (filtroAno && formatAno(filme.releasedate) !== parseInt(filtroAno)) {
+      return false
+    }
     return true
   }
 
@@ -68,23 +61,26 @@ const Catalogo: React.FC<ICatalogoProps> = ({ listaFilmes }) => {
     <section className={Style.catalogo}>
       <div className="container">
         <h1>Filmes</h1>
-
-        {/* Filtros */}
         <div className={Style.gridCatalogo}>
           <select value={filtroGenero} onChange={(e) => setFiltroGenero(e.target.value)}>
             <option value="">Gênero</option>
-            {Array.from(new Set(todosOsFilmes.map((filme) => filme.genre))).map((genre, index) => (
-              <option key={index} value={genre}>
-                {genre}
-              </option>
-            ))}
+            {Array.from(new Set(concatFilmes.map((data) => data.genre))).map(
+              (genre, index) => (
+                <option key={index} value={genre}>
+                  {genre}
+                </option>
+              )
+            )}
           </select>
           <select value={filtroAno} onChange={(e) => setFiltroAno(e.target.value)}>
             <option value="">Ano</option>
             {Array.from(
               { length: new Date().getFullYear() - 2023 },
               (_, index) => (
-                <option key={index} value={(new Date().getFullYear() - index).toString()}>
+                <option
+                  key={index}
+                  value={(new Date().getFullYear() - index).toString()}
+                >
                   {new Date().getFullYear() - index}
                 </option>
               )
@@ -95,41 +91,52 @@ const Catalogo: React.FC<ICatalogoProps> = ({ listaFilmes }) => {
               type="text"
               placeholder="Pesquisar nome..."
               value={pesquisa}
-              onChange={(e) => setPesquisa(e.target.value)}
+              onChange={({ target }) => setPesquisa(target.value)}
             />
-            <button onClick={handleBuscar}>
+            <button onClick={handlePesquisa}>
               <IoSearchSharp />
             </button>
           </div>
         </div>
-
         {isLoading ? (
           <p className={Style.Carregando}>Carregando...</p>
         ) : (
           <div>
-            <div className={Style.areaTitleCatalogoFilmeAno}>
-              <h2>{titulo}</h2>
-              <span></span>
-            </div>
-
-            <div className={Style.gridFilmesCatalogo}>
-              {(resultadosPesquisa.length > 0
-                ? resultadosPesquisa
-                : todosOsFilmes.filter(filtrarFilmes)
-              )
-                .sort(
-                  (a, b) =>
-                    new Date(b.releasedate).getTime() - new Date(a.releasedate).getTime()
-                )
-                .map((filme) => (
-                  <div key={filme.id}>
-                    <CardFilme data={filme} slide="catalogo" />
-                  </div>
-                ))}
-            </div>
-
-            {resultadosPesquisa.length === 0 &&
-              todosOsFilmes.filter(filtrarFilmes).length === 0 && (
+            {(filtroPesquisa.length > 0 ||
+              concatFilmes.filter(filtrarFilmes).length > 0) && (
+              <>
+                <div className={Style.areaTitleCatalogoFilmeAno}>
+                  <h2>
+                    {filtroGenero
+                      ? `Gênero: ${filtroGenero}`
+                      : filtroAno
+                      ? `Ano: ${filtroAno}`
+                      : ultimaPesquisa
+                      ? `Todos os resultados de: ${ultimaPesquisa}`
+                      : 'TUDO'}
+                  </h2>
+                  <span></span>
+                </div>
+                <div className={Style.gridFilmesCatalogo}>
+                  {(filtroPesquisa.length > 0
+                    ? filtroPesquisa
+                    : concatFilmes.filter(filtrarFilmes)
+                  )
+                    .sort(
+                      (a, b) =>
+                        new Date(b.releasedate).getTime() -
+                        new Date(a.releasedate).getTime()
+                    )
+                    .map((data) => (
+                      <div key={data.id}>
+                        <CardFilme data={data} slide="catalogo" />
+                      </div>
+                    ))}
+                </div>
+              </>
+            )}
+            {filtroPesquisa.length === 0 &&
+              concatFilmes.filter(filtrarFilmes).length === 0 && (
                 <p className={Style.CatalogoVazio}>Nenhum filme encontrado</p>
               )}
           </div>
